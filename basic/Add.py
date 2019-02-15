78fd62ce1ebbc88492770c882e51db3f8e45e9ac
monotonewang <zgjxfuqingwang@gmail.com>
2019-02-15 16:34:34 +0800

    添加注释

--- basic/Add.py
+++ basic/Add.py
@@ -1,25 +1,32 @@
-print 45678+0x12fd2
 
-print 'Learn Python in imooc.'
+# 这是单行注释
 
-print 100<99
+"""
+ 这是多行注释
+ 这是多行注释
+"""
+print(45678 + 0x12fd2)
+
+print('Learn Python in imooc.')
+
+print(100 < 99)
 
 # print 0xff==255
 
-print 0xff==255
+print(0xff == 255)
 
 x1 = 1
 d = 3
 n = 100
-x100 = x1+(n-1)*d
-s = (x1+x100) *n/2
-print s
+x100 = x1 + (n - 1) * d
+s = (x1 + x100) * n / 2
+print(s)
 
 s0 = 'Python was started in 1989 by "Guido".'
-print s0
+print(s0)
 
 s1 = 'Python is free and easy to learn.'
-print s1
+print(s1)
 
-print r'''"To be, or not to be": that is the question.'
-Whether it's nobler in the mind to suffer.'''
+print(r'''"To be, or not to be": that is the question.'
+Whether it's nobler in the mind to suffer.''')
