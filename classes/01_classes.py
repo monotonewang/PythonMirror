c2a46e6f7f4496ce0acbdbbd8382bc2ce275d15c
monotonewang <zgjxfuqingwang@gmail.com>
2019-02-20 08:02:46 +0800

    x

--- classes/01_classes.py
+++ classes/01_classes.py
@@ -1,19 +1,21 @@
 class Cat:
 
-    def __init__(self, height):
+    def __init__(self, height, weight):
         self.height = height
+        self.weight = weight
 
     def eat(self):
         print("%s cat eat" % self.name)
+        self.weight += 0.5  # 吃东西增加体重
 
     def __del__(self):
         print(" class is destory")
 
     def __str__(self):
-        return "cat height __str__ %s" % self.height
+        return "cat height __str__ %s weight %s" % (self.height, self.weight)
 
 
-cat = Cat(12)
+cat = Cat(12, 75)
 # 设置属性
 cat.name = "tom"
 cat.eat()
@@ -34,5 +36,3 @@ delattr(cat, 'height')  # 删除属性 'age'
 del cat
 
 print("the program is run end")
-
-
