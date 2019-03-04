abcf61861aed347d5fc73ce27b3a0cf79f4f388e
monotonewang <zgjxfuqingwang@gmail.com>
2019-03-04 20:27:31 +0800

    start one thread

--- test_thread/test_thread.py
+++ test_thread/test_thread.py
@@ -1,12 +1,54 @@
 import _thread
 
+import threading
+import time
+
+number = 1
+
 
 def print_time(thread_name, delay):
-    print("%s,%s" % (thread_name, delay))
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print("%s,%s count=%d" % (thread_name, time.ctime(time.time()), count))
 
 
-_thread.start_new_thread(print_time, ("Thread-2", 4,))
+def test():
+    global number
+    while number < 100:
+        number += 1
+        print("test=%d" % number)
+        pass
 
-# _thread.start_new_thread(print_time)
 
-# _thread.start_new_thread(print_time("x", 1),("a","b"))
+def test1():
+    global number
+    while number < 100:
+        number += 1
+        print("test1=%d" % number)
+        pass
+
+
+def main():
+    try:
+        _thread.start_new_thread(print_time("Thread-2", 0.1))
+    except Exception as result:
+        pass
+    finally:
+        pass
+
+    thread = threading.Thread(target=test)
+    thread1 = threading.Thread(target=test1)
+    thread.start()
+    thread1.start()
+
+    while True:
+        length = len(threading.enumerate())
+        print("current run thread %s" % length)
+        if length <= 1:
+            break
+
+
+if __name__ == "__main__":
+    main()
